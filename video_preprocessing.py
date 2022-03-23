3bbb37be4f6dd289501b45477ca23ce570c51543
Niranjan <niranjan4r@gmail.com>
2022-03-23 14:18:18 +0530

    added gaussian (comment)

--- video_preprocessing.py
+++ video_preprocessing.py
@@ -1,6 +1,8 @@
 import cv2
 
 def apply_preprocessing(image):
+    # gaussian = cv2.GaussianBlur(image, (7, 7), 0)
+    # gray = cv2.cvtColor(gaussian, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     equalized = cv2.equalizeHist(gray)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
